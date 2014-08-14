1d2ffcb691824f96c709dd63b6977a9da7b044a4
Guillaume DOTT <guillaume+github@dott.fr>
2014-08-14 17:48:21 +0200

    Replace `attr_accessor` with instance variable
    The `features` hash does not have to be accessed from outside the
    repository class.

--- lib/flop/repository/memory.rb
+++ lib/flop/repository/memory.rb
@@ -1,18 +1,16 @@
 module Flop
   module Repository
     class Memory
-      attr_accessor :features
-
       def initialize
-        self.features = Hash.new(false)
+        @features = Hash.new(false)
       end
 
       def get(name)
-        features[name]
+        @features[name]
       end
 
       def set(name, value)
-        features[name] = value
+        @features[name] = value
       end
     end
   end
